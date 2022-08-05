14f2d7143f436379cbb6c10e015ca8ce0e6075e5
Khanh1234 <your@example.com>
2022-08-05 10:36:47 +0700

    update

--- src/Templates/AdminTemplate.tsx
+++ src/Templates/AdminTemplate.tsx
@@ -4,10 +4,27 @@ import { Link, Navigate } from "react-router-dom";
 import Dashboard from "../Pages/Admin/Dashboard/Dashboard";
 import Film from "../Pages/Admin/Film/Film";
 import ShowTime from "../Pages/Admin/ShowTime/ShowTime";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import { useDispatch } from "react-redux";
 
 const { TabPane } = Tabs;
 
 const AdminTemplate = () => {
+  const {currentUser} = useSelector(
+    (state:RootState)=>state.auth
+  )
+
+  if(localStorage.length===0){
+    alert("Bạn chưa đăng nhập !");
+    return  <Navigate to="/"/>
+    }
+ if(currentUser.maLoaiNguoiDung !== "QuanTri"){
+  alert("Bạn không có quyền truy cập trang này");
+  return  <Navigate to="/"/>
+ }
+
+
   return (
     <div className="card-container" style={{marginTop:"100px"}}>
       <Tabs type="card">
